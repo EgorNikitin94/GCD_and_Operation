950cf84e689cc4a3996397bdec8164f778662214
Егор Никитин <Egornik1994@yandex.ru>
2021-01-29 00:11:12 +0300

    Initial Commit

---
+++ GCD and Operation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD and Operation
+//
+//  Created by Егор Никитин on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
